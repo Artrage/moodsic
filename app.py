6c77fe36fe546f15527196a971ce13c7f627b308
Art <art.balenok@gmail.com>
2020-10-06 11:44:28 +0000

    added logout functionality. fixed buttons layout and text alignment on reg an login pages

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def profile(username):
 @app.route("/logout")
 def logout():
     # remove user from session cookies
-    flash("You have been logged out")
+    flash("Logged out. Ciao!")
     session.pop("user")
     return redirect(url_for("login"))
 
